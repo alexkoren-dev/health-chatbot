8e46403437f80cfac24f4d52a0ffd515208aa035
alexkoren-dev <alexkoren222@gmail.com>
2023-07-17 20:15:11 +0530

    Update info.py

--- chatbot/info.py
+++ chatbot/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS=True
 EMAIL_HOST="smtp.gmail.com"
-EMAIL_HOST_USER="chitturisaikiran1210@gmail.com"
-EMAIL_HOST_PASSWORD="qaozhxqzurpiotmd"
+EMAIL_HOST_USER="Your Email"
+EMAIL_HOST_PASSWORD="Your Password"
 EMAIL_PORT=587
